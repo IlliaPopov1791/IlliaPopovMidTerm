712e74f2042c200c3efe0486ee5073c39bf9e4c2
IlliaPopov1791 <illia.m.popov@gmail.com>
2023-06-19 17:57:53 -0400

    First Commit: The initial build of the midterm.

---
+++ PopovModule1/src/main/java/illia/popov/n01421791/mid/MainActivity.java
@@ -0,0 +1,14 @@
+package illia.popov.n01421791.mid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
