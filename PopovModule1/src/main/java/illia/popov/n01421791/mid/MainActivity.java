b5ec2de83b1af5ced363ae9f6c7aee57dfbb89e3
IlliaPopov1791 <illia.m.popov@gmail.com>
2023-06-19 18:31:59 -0400

    Seventh Commit: Some files were modified. Switch was implemented in the application.

--- PopovModule1/src/main/java/illia/popov/n01421791/mid/MainActivity.java
+++ PopovModule1/src/main/java/illia/popov/n01421791/mid/MainActivity.java
@@ -3,6 +3,10 @@ package illia.popov.n01421791.mid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Switch switchButton = findViewById(R.id.switch1);
+        switchButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    showSnackbar(getString(R.string.switch_on_status));
+                } else {
+                    showSnackbar(getString(R.string.switch_off_status));
+                }
+            }
+        });
+    }
+    private void showSnackbar(String message) {
+        Snackbar.make(findViewById(android.R.id.content), message, Snackbar.LENGTH_SHORT).show();
     }
 }
