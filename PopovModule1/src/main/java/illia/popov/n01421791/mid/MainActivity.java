57632f67c6598d8e636f4d92a85241404cdfbdd4
IlliaPopov1791 <illia.m.popov@gmail.com>
2023-06-19 18:52:14 -0400

    Tenth Commit: Counter was implemented. When ImageButton clicked sends user a Toast Message with the counter.

--- PopovModule1/src/main/java/illia/popov/n01421791/mid/MainActivity.java
+++ PopovModule1/src/main/java/illia/popov/n01421791/mid/MainActivity.java
@@ -2,9 +2,13 @@ package illia.popov.n01421791.mid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.CompoundButton;
+import android.widget.ImageButton;
 import android.widget.Switch;
+import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 
@@ -26,8 +30,27 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+        ImageButton imageButton = findViewById(R.id.ill_image);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            int counter = 0;
+            final String[] imageNames = {"@drawable/ic_view", "@drawable/ic_sun", "@drawable/ic_humber", "@drawable/ic_macedonian"};
+
+            @Override
+            public void onClick(View v) {
+                counter+=1;
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+                displayToast("Counter: " + Integer.toString(counter));
+            }
+        });
     }
     private void showSnackbar(String message) {
         Snackbar.make(findViewById(android.R.id.content), message, Snackbar.LENGTH_SHORT).show();
+    }
+
+    private void displayToast(String msg)
+    {
+        Toast.makeText(getBaseContext(), msg,
+                Toast.LENGTH_SHORT).show();
     }
 }
---
+++ PopovModule1/src/main/java/illia/popov/n01421791/mid/SecondActivity.java
@@ -0,0 +1,14 @@
+package illia.popov.n01421791.mid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
